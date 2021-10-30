d0978759bfd9714d24457c91c29e5f2c8da723f7
Mahesh1602 <92970348+Mahesh1602@users.noreply.github.com>
2021-10-30 12:24:31 +0530

    Add files via upload

---
+++ 2 mehod for float basic data type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	float a,b,c,sum;
+	//initialization
+	a=1.2;
+	b=1.3;
+	c=1.4;
+	//calculation
+	sum=a+b+c;
+	//output
+	printf("sum of %f and %f and %f is:%f",a,b,c,sum);
+}
